3bb41ae122ebfff7697e51ed0751261c36c4173e
skullquake <ockert8080@gmail.com>
2023-08-08 19:04:01 +0000

    TestHarness   - Pass in response as array, not string in answering questionnaire within the javascript action
    Signed-off-by: Jorge Flores <jorge@entidad.io>

--- javascriptsource/questionnaire/actions/jsa_aries_agent_modules_questionnaire_sendAnswer.js
+++ javascriptsource/questionnaire/actions/jsa_aries_agent_modules_questionnaire_sendAnswer.js
@@ -24,6 +24,12 @@ export async function jsa_aries_agent_modules_questionnaire_sendAnswer(agent_key
 		if(agent_key==null)return Promise.reject("Invalid agent_key parameter");
 		if(questionnaireRecordId==null)return Promise.reject("Invalid questionnaireRecordId parameter");
 		if(response==null)return Promise.reject("Invalid response parameter");
+		try{
+			response=JSON.parse(response);
+		}catch(e){
+
+			return Promise.reject("Invalid response parameter, not a valid JSON: "+e.toString());
+		}
 		let agent=support.cache.get(agent_key);
 		if(agent==null)return Promise.reject("Agent not found in cache");
 		return Promise.resolve(JSON.stringify(
