3acec0ad9edec4779d4e51626e6efc9f780bc27c
Jorge Flores <jorge@entidad.io>
2023-10-25 22:02:40 -0700

    1. upgraded to Studio Pro 9.24.9 2. UI improvements to credential exchange and credential presentation screens 3. logic to set wallet label to be user friendly
    Signed-off-by: Jorge Flores <jorge@entidad.io>

---
+++ javascriptsource/questionnaire/node_modules/@aries-framework/core/build/modules/oob/repository/outOfBandRecordMetadataTypes.js
@@ -0,0 +1,9 @@
+"use strict";
+Object.defineProperty(exports, "__esModule", { value: true });
+exports.OutOfBandRecordMetadataKeys = void 0;
+var OutOfBandRecordMetadataKeys;
+(function (OutOfBandRecordMetadataKeys) {
+    OutOfBandRecordMetadataKeys["RecipientRouting"] = "_internal/recipientRouting";
+    OutOfBandRecordMetadataKeys["LegacyInvitation"] = "_internal/legacyInvitation";
+})(OutOfBandRecordMetadataKeys = exports.OutOfBandRecordMetadataKeys || (exports.OutOfBandRecordMetadataKeys = {}));
+//# sourceMappingURL=outOfBandRecordMetadataTypes.js.map
